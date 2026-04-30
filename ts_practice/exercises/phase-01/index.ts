9616765b8563290f359b847a4b5bec0643782e23
yangwenhao11 <yangwenhao11@100.me>
2026-04-30 17:09:13 +0800

    exercise

--- ts_practice/exercises/phase-01/index.ts
+++ ts_practice/exercises/phase-01/index.ts
@@ -25,3 +25,15 @@ export function safeParse(json: string): User | null {
   if (isUser(data)) return data;
   return null;
 }
+
+export type Event = 'click' | 'hover' | 'press';
+
+export function handle(e:Event){
+    return e;
+}
+
+const colors = ['red', 'green', 'blue'] as const
+export type Color = typeof colors[number];
+
+let c1:Color = 'red';
+// let c2:Color = 'gray';
