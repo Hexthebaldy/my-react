2abdc6fd1623221fd533e5bde8ea1ec855f4c52d
yangwenhao11 <yangwenhao11@100.me>
2026-04-30 15:57:15 +0800

    exercise

--- ts_practice/exercises/phase-01/index.ts
+++ ts_practice/exercises/phase-01/index.ts
@@ -1,3 +1,27 @@
-export function repeat(s:string, n:number) {
-  return Array(n).fill(s).join('')
+export function repeat(s: string, n: number) {
+  return Array(n).fill(s).join("");
+}
+
+export interface User {
+  id: number;
+  name: string;
+}
+
+export function safeParse(json: string): User | null {
+  let data: unknown = JSON.parse(json);
+  function isUser(x: unknown): x is User {
+    if (
+      data instanceof Object &&
+      data !== null &&
+      "id" in data &&
+      typeof data.id === "number" &&
+      "name" in data &&
+      typeof data.name === "string"
+    ) {
+      return true;
+    }
+    return false;
+  }
+  if (isUser(data)) return data;
+  return null;
 }
