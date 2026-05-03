2d7b128d59ac25ef8a14937c7e92ae559e2c9871
yang <7thwesk@gmail.com>
2026-05-03 18:51:00 +0800

    exercise

--- ts_practice/exercises/phase-03/index.ts
+++ ts_practice/exercises/phase-03/index.ts
@@ -6,3 +6,38 @@ export class Account {
     constructor(id:string, balance:number) { this.id = id; this.balance = balance }
     deposit(n:number) { this.balance += n }
 }
+
+export abstract class Shape{
+    abstract area():number;
+    describe(){
+        console.log('shape with area X')
+    }
+}
+
+export class Circle extends Shape {
+    r:number
+    static PI = 3.14;
+
+    constructor(r:number){
+        super();
+        this.r = r;
+    }
+
+    area():number{
+        return Circle.PI * this.r*this.r ;
+    }
+}
+
+export class Rectangle extends Shape{
+    width:number;
+    height:number;
+    constructor(width:number,height:number){
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    area():number{
+        return this.width*this.height;
+    }
+}
