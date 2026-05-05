5f419792b22c873b8bb10ca6d0ab1fb063b21b04
yang <7thwesk@gmail.com>
2026-05-05 18:05:54 +0800

    exercise

--- ts_practice/exercises/phase-04/index.ts
+++ ts_practice/exercises/phase-04/index.ts
@@ -1,3 +1,9 @@
 export function getProp<T,K extends keyof T>(obj:T,key:K):T[K]{
     return obj[key];
 }
+
+export function isNonEmptyArray<T>(arr:T[]):arr is [T,...T[]]{
+    if(arr.length) return true;
+    return false;
+}
+
