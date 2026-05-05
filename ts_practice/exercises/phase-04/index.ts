a7b46b7e406b15fce073cc7c4af0b505dcb8466a
yang <7thwesk@gmail.com>
2026-05-05 19:26:12 +0800

    exercise

--- ts_practice/exercises/phase-04/index.ts
+++ ts_practice/exercises/phase-04/index.ts
@@ -11,3 +11,32 @@ export function assertDefined<T>(x:T|null|undefined):asserts x is T{
     if(x == undefined) throw Error("x is null or undefined");
 }
 
+export type AsyncState<T,E> = 
+    {status:'idle'}
+  | {status:'loading'}
+  | {status:'success',res:T}
+  | {status:'error',err:E}
+
+export type AsyncAction<T,E> = 
+    {type:'FETCH'}
+  | {type:'SUCCESS',res:T}
+  | {type:'ERROR',err:E}
+  | {type:'RESET'}
+
+export function reduce<T,E>(s:AsyncState<T,E>,action:AsyncAction<T,E>):AsyncState<T,E>{
+    switch (action.type){
+        case 'FETCH':
+            return {status:'loading'}
+        case 'SUCCESS':
+            return {status:'success',res: action.res};
+        case 'ERROR': 
+            return {status:'error',err:action.err}
+        case 'RESET':
+            return {status:'idle'}
+    }
+}
+
+export function setState<T,E>(s:AsyncState<T,E>,action:(s:AsyncState<T,E>)=>{}):AsyncState<T,E>{
+    action(s);
+    return s;
+}
